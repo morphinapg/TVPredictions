b53ae3224c30f4f7a04fe2820b52f71976128fb7
morphinapg <52674976+morphinapg@users.noreply.github.com>
2020-06-17 22:35:42 -0400

    Slight fix for prediction breakdown

--- TV Ratings Predictions/ShowBreakdown.xaml.cs
+++ TV Ratings Predictions/ShowBreakdown.xaml.cs
@@ -466,7 +466,7 @@ namespace TV_Ratings_Predictions
             {
                 double shift = change != 0 ? (CurrentOdds - BaseOdds) / change : 1;
                 if (Math.Round(Math.Abs(CurrentOdds - BaseOdds), 4) == 0) shift = 0;
-                double oldEx = 1, exponent = 1, increment = (shift < 1) ? 0.0001 : -0.0001;
+                double oldEx = 1, exponent = 1, increment = (Math.Abs(shift) < 1) ? 0.0001 : -0.0001;
 
                 double oldChange = change;
 
